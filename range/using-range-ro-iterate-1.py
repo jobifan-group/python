ca8d4cafb1326d55c3aa83f4eb369bb115fe3950
jobifan-group <jobifan.python@gmail.com>
2019-11-02 22:49:01 +0330

    Using range method in python.

---
+++ range/using-range-ro-iterate-1.py
@@ -0,0 +1,8 @@
+print("We are going to print numbers less than 10")
+for num in range(10):
+    print(num, end=" ")
+
+print("\n")
+
+for num in range(0, 10):
+    print(num, end=" ")
---
+++ range/using-range-ro-iterate-2.py
@@ -0,0 +1,20 @@
+print("We are going to print numbers less than 10")
+for num in range(10):
+    print(num, end=" ")
+
+print("\n")
+
+for num in range(0, 10):
+    print(num, end=" ")
+
+print("\nWe are going to print even numbers from 10 to 20")
+for num in range(10, 21, 2):
+    print(num, end=" ")
+
+print("\nWe are going to print odd numbers from 11 to 21")
+for num in range(11, 22, 2):
+    print(num, end=" ")
+
+print("\nWe are going to print numbers from 11 to 21, by step 5")
+for num in range(11, 22, 5):
+    print(num, end=" ")
